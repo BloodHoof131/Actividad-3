2f2ddd4680e98e83b6968810fe77f7957624efda
BloodHoof131 <blaverde@unal.edu.co>
2026-05-17 16:38:09 -0500

    Commit

--- POO/Ejercicio 8.2 pagina 483/Notas.java
+++ POO/Ejercicio 8.2 pagina 483/Notas.java
@@ -1,77 +1,54 @@
 package Notas;
 
-/**
- * Esta clase denominada Notas define un array de notas numéricas de
- * tipo double.
- * @version 1.2/2020
- */
+
 public class Notas {
 
-    double[] listaNotas; /* Atributo que identifica un array de notas de
-    tipo double */
+    double[] listaNotas; 
 
-    /**
-     * Constructor de la clase Notas, instancia un array con 5 notas de
-     * tipo double
-     */
+    
     public Notas() {
 
-        listaNotas = new double[5]; // Crea un array de 5 notas
+        listaNotas = new double[5]; 
     }
 
-    /**
-     * Método que calcula el promedio de notas
-     * @return El promedio de notas calculado
-     */
+   
     double calcularPromedio() {
 
         double suma = 0;
 
-        for (int i = 0; i < listaNotas.length; i++) { // Se recorre el array
-            suma = suma + listaNotas[i]; // Suma las notas del array
+        for (int i = 0; i < listaNotas.length; i++) {
+            suma = suma + listaNotas[i]; 
         }
 
-        /* Obtiene el promedio como la división de la suma de notas
-        sobre el total de notas */
         return (suma / listaNotas.length);
     }
 
-    /**
-     * Método que calcula la desviación estándar del array de notas
-     * @return La desviación estándar del array de notas
-     */
+    
     double calcularDesviación() {
 
-        double prom = calcularPromedio(); /* Invoca el método para
-        calcular el promedio */
+        double prom = calcularPromedio(); 
 
         double suma = 0;
 
         for (int i = 0; i < listaNotas.length; i++) {
 
-            // Aplica fórmula para la sumatoria de elementos
+            
             suma += Math.pow(listaNotas[i] - prom, 2);
         }
 
-        return Math.sqrt(suma / listaNotas.length); /* Retorna el cálculo
-        final de la desviación */
+        return Math.sqrt(suma / listaNotas.length); 
     }
 
-    /**
-     * Método que calcula el valor menor del array de notas
-     * @return El valor menor del array de notas
-     */
+    
     double calcularMenor() {
 
-        double menor = listaNotas[0]; /* Define una variable como la
-        nota menor */
+        double menor = listaNotas[0]; 
 
-        for (int i = 0; i < listaNotas.length; i++) { // Se recorre el array
+        for (int i = 0; i < listaNotas.length; i++) { 
 
             if (listaNotas[i] < menor) {
 
-                /* Si un elemento del array es menor que el menor actual,
-                se actualiza su valor */
+                
                 menor = listaNotas[i];
             }
         }
@@ -79,21 +56,16 @@ public class Notas {
         return menor;
     }
 
-    /**
-     * Método que calcula el valor mayor del array de notas
-     * @return El valor mayor del array de notas
-     */
+    
     double calcularMayor() {
 
-        double mayor = listaNotas[0]; /* Define una variable como la
-        nota mayor */
+        double mayor = listaNotas[0]; 
 
-        for (int i = 0; i < listaNotas.length; i++) { // Se recorre el array
+        for (int i = 0; i < listaNotas.length; i++) { 
 
             if (listaNotas[i] > mayor) {
 
-                /* Si un elemento del array es mayor que el mayor actual,
-                se actualiza su valor */
+                
                 mayor = listaNotas[i];
             }
         }
